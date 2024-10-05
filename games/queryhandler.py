6a8b24109d0402f9966ae93b08d00f30236fbb2e
Andre Franca <andreffranca@gmail.com>
2024-10-05 14:06:54 +0100

    query obj

--- games/queryhandler.py
+++ games/queryhandler.py
@@ -11,13 +11,13 @@ class QueryHandler:
         self.action_type = action_type
         self.player_info = player_info
 
-        self.client = get_client(player_info)
+        self.query_object = get_client_query(player_info)
 
     def handle_response(self, response: str):
         pass
 
     def get_query_object(self) -> Callable:
-        return self.client.query
+        return self.query_object
 
     def query_player(self, player_id: str, prompt: str):
         query_object = self.get_query_object()
