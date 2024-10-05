fa31f271ac04bbd1ab0ae3aaf580d65545ff2736
Andre Franca <andreffranca@gmail.com>
2024-10-05 11:40:04 +0100

    gamedef

--- games/gamedef.py
+++ games/gamedef.py
@@ -4,8 +4,3 @@ from pydantic import BaseModel
 class Gamedef(BaseModel):
     name: str
     description: str
-    rules: str
-    instructions: str
-    examples: str
-    input_format: str
-    output_format: str
